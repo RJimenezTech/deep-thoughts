c8bc1017609a08d592ebce6d7f0f1263442d9613
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-19 17:51:29 -0500

    added reaction functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {useQuery} from '@apollo/client';
-import {QUERY_THOUGHT} from '../utils/queries';
-import ReactionList from '../components/ReactionList';
+import { useParams } from 'react-router-dom';
 
-const SingleThought = props => {
-  const {id: thoughtId} = useParams();
-  const {loading, data} = useQuery(QUERY_THOUGHT, {
-    variables: {id: thoughtId}
-  })
+import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId },
+  });
 
   const thought = data?.thought || {};
 
@@ -17,21 +22,25 @@ const SingleThought = props => {
   }
 
   return (
-<div>
-  <div className="card mb-3">
-    <p className="card-header">
-      <span style={{ fontWeight: 700 }} className="text-light">
-        {thought.username}
-      </span>{' '}
-      thought on {thought.createdAt}
-    </p>
-    <div className="card-body">
-      <p>{thought.thoughtText}</p>
-    </div>
-  </div>
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {thought.username}
+          </span>{' '}
+          thought on {thought.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{thought.thoughtText}</p>
+        </div>
+      </div>
 
-  {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-</div>
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+    </div>
   );
 };
 
